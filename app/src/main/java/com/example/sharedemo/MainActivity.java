e1475fa170d7c6c08b96b625a287ac21096ff1fe
wuweishan1995 <wws@gzqianyin.cn>
2020-09-07 16:36:41 +0800

    *)工程初始化

---
+++ app/src/main/java/com/example/sharedemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sharedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recycler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recycler = findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+    }
+}
